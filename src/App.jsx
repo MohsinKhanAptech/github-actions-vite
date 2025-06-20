22b479051fe0c3adaa8a7764c54c1c6bb0ecfdca
MohsinKhanAptech <muhammadmohsinkhan.aptech@gmail.com>
2025-06-20 16:47:45 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="*:text-blue-900">
+      <h1 className="text-9xl font-bold">Lorem ipsum dolor sit amet.</h1>
+      <p>Lorem ipsum dolor sit amet.</p>
+    </div>
+  )
+}
+
+export default App
